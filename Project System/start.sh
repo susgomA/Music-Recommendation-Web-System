eb3f64fd386d7862a4910214f18ab6f07c38ac06
ChocoSundaee <118056210+susgomA@users.noreply.github.com>
2025-12-11 00:02:12 +0800

    Update start.sh

--- Project System/start.sh
+++ Project System/start.sh
@@ -1,9 +1,6 @@
+# start.sh
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status
 set -o errexit
 
-# Command to start the Gunicorn web server.
-# --workers 4: Starts 4 worker processes for concurrency.
-# --bind 0.0.0.0:$PORT: Binds the server to the dynamic port ($PORT) assigned by Render.
-# app:app: Specifies the entry point: module 'app' (your app.py) and variable 'app' (your Flask application instance).
-gunicorn --workers 4 --bind 0.0.0.0:$PORT app:app
+# Use the full path here: FolderName.Filename:AppInstanceName
+gunicorn --workers 4 --bind 0.0.0.0:$PORT 'Project System.app:app'
